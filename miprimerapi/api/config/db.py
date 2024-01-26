d9ce636d47df020fbf1dc31663008b821c1a3701
Cramcat639 <marc639@outlook.com>
2024-01-26 18:57:00 +0100

    Version 1.0.2
    Entorno virtual actualizado.
    db.py modificado para rutas relativas.

--- miprimerapi/api/config/db.py
+++ miprimerapi/api/config/db.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine, MetaData
+import os
 
-engine = create_engine("sqlite:///C:/Users/admin/Desktop/Mobiles M8/api/db/Crud2.db")
 
+# Obtiene la ruta absoluta del directorio del script
+script_directory = os.path.abspath(os.path.dirname(__file__))
+
+# Combina la ruta del script con la ruta relativa a la base de datos
+database_path = os.path.join(script_directory,"..", "db", "Crud2.db")
+
+# Crea el motor de la base de datos
+engine = create_engine(f"sqlite:///{database_path}")
 meta = MetaData()
 
 conn = engine.connect()
