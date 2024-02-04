4d09c27fbef98a9c944203d7b8fe24e5d75bf265
Cramcat639 <marc639@outlook.com>
2024-02-04 13:04:40 +0100

    Version 1.0.3 V2
    Rebuilt

---
+++ miprimerapi/app/build/generated/source/buildConfig/debug/com/cramcat/platform/CRUDapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cramcat.platform.CRUDapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cramcat.platform.CRUDapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
